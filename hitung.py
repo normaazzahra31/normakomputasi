b968c7ff95e712a9c97f4cb278121d81e35ec271
normaazzahra31 <normawidyasti@gmail.com>
2024-09-23 10:06:33 +0700

    Update hitung.py

--- hitung.py
+++ hitung.py
@@ -4,5 +4,10 @@ x = st.number_input("Masukkan angka")
 sx = st.text_input("Satuan", "C")
 st.write ("Anda memasukkan", x, ' ', sx)
 sy = st.text_input("Dikonversi ke", "C")
-
+y = 0
+if (sx == 'C'):
+  if (sy == 'C'):
+    y = x
+  elif(sy == 'F'):
+    
 st.write (x, ' ', sx, '=...' sy)
