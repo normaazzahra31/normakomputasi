70bb79f1109bf33ced8ee5704bc07239dbf3511e
normaazzahra31 <normawidyasti@gmail.com>
2024-09-23 09:57:49 +0700

    Update hitung.py

--- hitung.py
+++ hitung.py
@@ -3,3 +3,6 @@ import streamlit as.st
 x = st.number_input("Masukkan angka")
 sx = st.text_input("Satuan", "C")
 st.write ("Anda memasukkan", x, ' ', sx)
+sy = st.text_input("Dikonversi ke", "C")
+
+st.write (x, ' ', sx, '=...' sy)
