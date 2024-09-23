db2dee094cda130c6e36644dca4217914b6450d0
normaazzahra31 <normawidyasti@gmail.com>
2024-09-23 10:29:05 +0700

    Update hitungg.py

--- hitungg.py
+++ hitungg.py
@@ -1,3 +1,15 @@
+import streamlit as st
+
+x = st.number_input("Masukkan angka")
+sx = st.text_input("Satuan", "C")
+st.write ("Anda memasukkan", x, ' ', sx)
+sy = st.text_input("Dikonversi ke", "C")
+y = 0
+if (sx == 'C'):
+  if (sy == 'C'):
+    y = x
+  elif(sy == 'F'):
+      
 def celsius_to_fahrenheit(celsius):
     return (celsius * 9/5) + 32
 
